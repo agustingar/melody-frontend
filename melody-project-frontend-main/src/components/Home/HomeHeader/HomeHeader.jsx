882239360171de1fe71af869dc663cf0ba9bde00
Walber Melo <dev4wm@gmail.com>
2022-11-29 11:57:42 +0000

    fix(home=: Add user name header app

--- melody-project-frontend-main/src/components/Home/HomeHeader/HomeHeader.jsx
+++ melody-project-frontend-main/src/components/Home/HomeHeader/HomeHeader.jsx
@@ -1,17 +1,25 @@
 import "./HomeHeader.css";
+import "../../Top/Top.css";
 import React from "react";
+import { useGetUserQuery } from "../../../redux/services/melodyApi";
 
 function HomeHeader() {
-  const {
-    data: user,
-    isFetching: userFetching,
-    error: userError,
-  } = useGetUserQuery();
+  const { data, isFetching, error } = useGetUserQuery();
+
+  if (isFetching)
+    return (
+      <div className="loading-box">
+        <div className="loading_bar"></div>
+        <p className="loading_text">Loading</p>
+      </div>
+    );
+
+  if (error) return <div>Error</div>;
 
   return (
     <div className="header-container-home">
       <div className="header-user--name">
-        <b>Hello</b> {user?.user.name}!
+        <b>Hello</b> {data?.user.name}!
       </div>
     </div>
   );
